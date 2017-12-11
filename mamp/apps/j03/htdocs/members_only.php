0d84fcbc6581f3f584bf072ff565fb491b29b270
Magalie YANG <myang@e2r2p10.42.fr>
2017-12-11 13:42:52 +0100

    id ok

--- mamp/apps/j03/htdocs/members_only.php
+++ mamp/apps/j03/htdocs/members_only.php
@@ -1,5 +1,17 @@
 <?php
-	$_SERVEUR['pseudo'] = "zaz";
+echo "On cherche le pseudo...";
+
+	if (!isset($_SERVER['PHP_AUTH_USER']))
+	{
+		header('WWW-Authenticate: Basic');
+		header('HTTP/1.0 401 Unauthorized');
+		echo "fail\n";
+	}
+	else
+	{
+		echo "<p>, {$_SERVER['PHP_AUTH_USER']}.</p>";
+		echo "<p>Votre mot de passe est {$_SERVER['PHP_AUTH_PW']}.</p>";
+	}
 	echo "On cherche le pseudo";
 	// header('Location: http://www.mysite.com/someotherpage.php')
 ?>
